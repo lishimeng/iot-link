f1998b38d8b1684f43ee38e0f75fc6b2054f34d8
lishimeng <alex.li@jxfinancial.cn>
2019-07-30 17:20:22 +0800

    vgo

---
+++ go.mod
@@ -0,0 +1,55 @@
+module github.com/lishimeng/iot-link
+
+go 1.12
+
+require (
+	github.com/BurntSushi/toml v0.3.1
+	github.com/Joker/jade v1.0.0 // indirect
+	github.com/Shopify/goreferrer v0.0.0-20181106222321-ec9c9a553398 // indirect
+	github.com/ajg/form v1.5.1 // indirect
+	github.com/astaxie/beego v1.12.0
+	github.com/aymerick/raymond v2.0.2+incompatible // indirect
+	github.com/eknkc/amber v0.0.0-20171010120322-cdade1c07385 // indirect
+	github.com/elazarl/go-bindata-assetfs v1.0.0
+	github.com/fatih/structs v1.1.0 // indirect
+	github.com/flosch/pongo2 v0.0.0-20190707114632-bbf5a6c351f4 // indirect
+	github.com/gavv/monotime v0.0.0-20190418164738-30dba4353424 // indirect
+	github.com/google/go-querystring v1.0.0 // indirect
+	github.com/gorilla/schema v1.1.0 // indirect
+	github.com/imkira/go-interpol v1.1.0 // indirect
+	github.com/iris-contrib/blackfriday v2.0.0+incompatible // indirect
+	github.com/iris-contrib/formBinder v5.0.0+incompatible // indirect
+	github.com/iris-contrib/go.uuid v2.0.0+incompatible // indirect
+	github.com/iris-contrib/httpexpect v0.0.0-20180314041918-ebe99fcebbce // indirect
+	github.com/jeanphorn/log4go v0.0.0-20190526082429-7dbb8deb9468
+	github.com/json-iterator/go v1.1.7 // indirect
+	github.com/jteeuwen/go-bindata v3.0.7+incompatible
+	github.com/k0kubun/colorstring v0.0.0-20150214042306-9440f1994b88 // indirect
+	github.com/kataras/golog v0.0.0-20190624001437-99c81de45f40 // indirect
+	github.com/kataras/iris v11.1.1+incompatible
+	github.com/kataras/pio v0.0.0-20190103105442-ea782b38602d // indirect
+	github.com/klauspost/compress v1.7.4 // indirect
+	github.com/klauspost/cpuid v1.2.1 // indirect
+	github.com/lib/pq v1.2.0
+	github.com/lishimeng/go-connector v0.0.0-20190730091325-932d9917f299
+	github.com/lishimeng/shutdown v0.0.0-20190619070718-c45a90ef55ea
+	github.com/mattn/go-colorable v0.1.2 // indirect
+	github.com/microcosm-cc/bluemonday v1.0.2 // indirect
+	github.com/moul/http2curl v1.0.0 // indirect
+	github.com/onsi/ginkgo v1.8.0 // indirect
+	github.com/onsi/gomega v1.5.0 // indirect
+	github.com/robertkrimen/otto v0.0.0-20180617131154-15f95af6e78d
+	github.com/ryanuber/columnize v2.1.0+incompatible // indirect
+	github.com/sergi/go-diff v1.0.0 // indirect
+	github.com/shurcooL/sanitized_anchor_name v1.0.0 // indirect
+	github.com/smartystreets/goconvey v0.0.0-20190710185942-9d28bd7c0945 // indirect
+	github.com/xeipuuv/gojsonpointer v0.0.0-20180127040702-4e3ac2762d5f // indirect
+	github.com/xeipuuv/gojsonreference v0.0.0-20180127040603-bd5ef7bd5415 // indirect
+	github.com/xeipuuv/gojsonschema v1.1.0 // indirect
+	github.com/yalp/jsonpath v0.0.0-20180802001716-5cc68e5049a0 // indirect
+	github.com/yudai/gojsondiff v1.0.0 // indirect
+	github.com/yudai/golcs v0.0.0-20170316035057-ecda9a501e82 // indirect
+	github.com/yudai/pp v2.0.1+incompatible // indirect
+	google.golang.org/appengine v1.6.1 // indirect
+	gopkg.in/sourcemap.v1 v1.0.5 // indirect
+)
--- internal/connector/lorawan/lorawan.go
+++ internal/connector/lorawan/lorawan.go
@@ -2,9 +2,8 @@ package lorawan
 
 import (
 	"encoding/base64"
-	"fmt"
 	log "github.com/jeanphorn/log4go"
-	"github.com/lishimeng/go-connector/mqtt"
+	"github.com/lishimeng/go-connector/lorawan"
 	"github.com/lishimeng/iot-link/internal/connector"
 	"github.com/lishimeng/iot-link/internal/model"
 )
@@ -12,48 +11,29 @@ import (
 type connectorLoraWan struct {
 	Id               string
 	Name             string
-	Host             string
-	ClientId         string
-	DownLinkTopicTpl string
-	UpLinkTopicTpl   string
-	Session          *mqtt.Session
+	Connector        lorawan.Connector
 	Listener         connector.UpLinkListener
 	State            bool
 	StateDescription string
 }
 
-func New(id string, name string, mqttBroker string, mqttClientId string, topicUpLink string, topicDownLink string) (connector.Connector, error) {
+func New(id string, name string, broker string, clientId string, topicUpLink string, topicDownLink string) (connector.Connector, error) {
 
-	log.Debug("create mqtt connector[%d]", mqttBroker)
+	log.Debug("create mqtt connector[%d]", broker)
+
 	c := connectorLoraWan{
 		Id: id,
 		Name: name,
-		Host: mqttBroker,
-		ClientId: mqttClientId,
-		DownLinkTopicTpl: topicDownLink,
-		UpLinkTopicTpl: topicUpLink,
 		State: false,
 	}
 
-	var onConnect = func(s mqtt.Session) {
-		log.Debug("lora mqtt subscribe upLink topic:%s", c.UpLinkTopicTpl)
-		c.State = c.Session.State
-		c.Session.Subscribe(c.UpLinkTopicTpl, 0, nil)
-		c.State = c.Session.State
+	proxy, err := lorawan.New(broker, clientId, topicUpLink, topicDownLink, 0)
+	if err != nil {
+		return nil, err
 	}
-	var onConnLost = func(s mqtt.Session) {
-		log.Debug("lora mqtt lost connection")
-		c.Session.State = false
-		c.State = c.Session.State
-	}
-	c.Session = mqtt.CreateSession(c.Host, c.ClientId)
-
-	c.Session.OnConnected = onConnect
-	c.Session.OnLostConnect = onConnLost
-	c.Session.OnMessage = c.messageCallback
-
-	log.Debug("lora mqtt connect to broker %s", c.Host)
-	c.Session.Connect()
+	c.Connector = proxy
+	proxy.SetUpLinkListener(c.onMessage)
+	proxy.Connect()
 
 	var conn connector.Connector = &c
 	return conn, nil// TODO
@@ -77,7 +57,7 @@ func (c connectorLoraWan) GetID() string {
 }
 
 func (c connectorLoraWan) GetState() bool {
-	return c.State
+	return c.Connector.GetSession().State
 }
 
 func (c connectorLoraWan) GetName() string {
@@ -90,20 +70,12 @@ func (c *connectorLoraWan) SetListener(listener connector.UpLinkListener) {
 
 // 监听数据上传
 ///
-func(c *connectorLoraWan) messageCallback(mqSession mqtt.Session, topic string, mqttMsg []byte) {
-
-	log.Debug("receive lora upLink data %s", topic)
-	context := model.LinkMessage{}
-	payload, err := onDataUpLink(mqttMsg)
-	if err != nil {
-		return
-	}
-
-	// lorawan 业务数据base64格式
+func (c *connectorLoraWan) onMessage(payload lorawan.PayloadRx) {
 	rawData, err := base64.StdEncoding.DecodeString(payload.Data)
 	if err != nil {
 		return
 	}
+	context := model.LinkMessage{}
 	context.ApplicationID = payload.ApplicationID
 	context.DeviceID = payload.DevEUI
 	context.ApplicationName = payload.ApplicationName
@@ -127,13 +99,10 @@ func (c connectorLoraWan) DownLink(target model.Target, logicData []byte) {
 	// 业务数据部分必须为base64格式
 	raw := base64.StdEncoding.EncodeToString(logicData)
 
-	payload := PayloadTx{FPort: 2, Data: raw}
-	data := convertJsonDownLinkData(payload) // 序列化得到json类型data字符串
-
-	topic := fmt.Sprintf(c.DownLinkTopicTpl, target.AppId, target.DeviceId)
+	downLinkData := lorawan.PayloadTx{FPort: 2, Data: raw}
 
 	go func() {
-		err := c.Session.Publish(topic, 0, data)
+		err := c.Connector.DownLink(target.AppId, target.DeviceId, downLinkData)
 		if err != nil {
 			log.Debug(err)
 		}
